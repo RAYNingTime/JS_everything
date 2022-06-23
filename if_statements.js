6ff31c4b1ee7f9397d6f7946445d0b9abbabb64d
RAYNingTime <ivan0kosyakov@gmail.com>
2022-06-23 12:27:34 +0300

    if statements in JS

---
+++ if_statements.js
@@ -0,0 +1,31 @@
+// It's same as in C++/C/PHP
+
+var number = 5;
+
+if(number == 4)
+	console.log("It's four!");
+else if(number == 5)
+	console.log("It's five!");
+else 
+	console.log("It's not four or five!");
+
+
+
+/*
+Logic Gates
+
+Equality == *With type convesrion
+Strict Equality === *Without type convesrion
+
+NOT equality != *With type convesrion
+Strict NOT equality !== *Without type convesrion
+
+Greater >
+Greater or equal >=
+
+Less <
+Less or equal <=
+
+AND &&
+OR ||
+*/
