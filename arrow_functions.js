5a1a0c134cbf3bcb7a2b16744028056a3b4901de
RAYNingTime <ivan0kosyakov@gmail.com>
2022-06-23 14:13:50 +0300

    Arrow functions and prevention of theObject mutate

---
+++ arrow_functions.js
@@ -0,0 +1,48 @@
+
+/*
+	Function form can be simplified
+
+
+This function: 
+var magic = function(){
+	return new Date();
+}
+
+Actually the same as this one:
+var magic = () => {
+	return new Date();
+}
+
+And same and this one:*/
+
+var magic = () => new Date();
+
+//How to write arrow functions with parameters?
+
+var myConcat = (arr1, arr2) => arr1.concat(arr2);
+
+console.log(myConcat([1,2], [3,4,5]));
+
+
+// How to write higher Order Arrow Functions #1
+
+const realNumberArray = [4, 5.6, 3.14, 42, 6, 8.34, -2];
+
+const squareList =  (arr) => {
+	const squaredIntegers = arr.filter(num => Number.isInteger(num) && num>0).map(x => x * x);
+	return squaredIntegers;
+}
+
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
+
+// How to write higher Order Arrow Functions #2
+
+const increment = (function() {
+	return function increment(number, value = 1) {
+		return number + value;
+	};
+})();
+
+console.log(increment(5,2));
+console.log(increment(5));
---
+++ object_mutation.js
@@ -0,0 +1,14 @@
+
+const MATH_CONSTANTS = {
+	PI: 3.14
+};
+
+//To prevent changing object we use function called Object.freeze
+
+Object.freeze(MATH_CONSTANTS);
+
+try {
+	MATH_CONSTANTS.PI = 99;
+} catch( ex ) {
+	console.log(ex);
+}
