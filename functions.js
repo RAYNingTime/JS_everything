088f2332bdb35f3843d7ab142ccf8f5758287e44
RAYNingTime <ivan0kosyakov@gmail.com>
2022-06-23 12:16:07 +0300

    Functions and some another replacements in variables

---
+++ functions.js
@@ -0,0 +1,15 @@
+//Function
+//In JS you don't need to write a type of the return variables
+function ourReusableFunction(/* Arguments */){
+	//Code
+
+	//Return if needed	
+}
+
+ourReusableFunction();
+
+//In JS there is also way for creating global variables and local one
+//To create GLOBAL variable - you should put it outside of the functions and it will me accessable EVERYWHERE
+//To create LOCAL variable - you should put it inside of the functions and it will me accessable ONLY inside of the function
+
+//We can use JSON.stringify(*array*) to create a string from array
--- variables.js
+++ variables.js
@@ -1,5 +1,26 @@
 //JavaScript have Case Sensitivity in variables
 
+/*
+	Data Types (7 different types):
+	undefiend, null, boolean, string, symbol, number and object
+*/
+
+// === THERE IS 3 WAYS TO DECLARE VARIABLE === //
+
+//First way to declare variable
+//Is going to be used all among the program
+var myName = "Ivan"
+
+myName = 8
+
+//Second way to declare variable
+//Is going to be used only in scope where you declared that
+let ourName = "UNIC"
+
+//Third way to declare variable
+//Cannot be changed (Regular const)
+const pi = 3.14
+
 //-----------------
 
 //Initializate Variable
