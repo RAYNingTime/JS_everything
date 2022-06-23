cf6231ac4446a175bbbcdbcf2383ea3768e4afdd
RAYNingTime <ivan0kosyakov@gmail.com>
2022-06-23 13:10:39 +0300

    Random numbers in JS

---
+++ randomNumbers.js
@@ -0,0 +1,21 @@
+
+//Math.random - used to generate number from 0 to 1
+//Math.floor - used to round number to lowest
+
+//TIP: To generate number in range that you need you can multiply random floor number
+
+var randomNumber = Math.random();
+
+console.log(randomNumber);
+
+//Random number from 0 to 19
+randomNumber = Math.floor(randomNumber * 20);
+console.log(randomNumber);
+
+//To generate number in your range we can use this command:
+
+function ourRandomRange(ourMin, ourMax) {
+	return Math.floor(Math.random() * (ourMax - ourMin + 1)) + ourMin;
+}
+
+console.log(ourRandomRange(5,10));
