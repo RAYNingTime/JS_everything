be32326207b765d2bbef1de74cd062d67c74df14
RAYNingTime <ivan0kosyakov@gmail.com>
2022-06-23 10:41:29 +0300

    Declare Variables

---
+++ datatypes_and_variables.js
@@ -0,0 +1,18 @@
+/*
+	Data Types (7 different types):
+	undefiend, null, boolean, string, symbol, number and object
+*/
+
+//First way to declare variable
+//Is going to be used all among the program
+var myName = "Ivan"
+
+myName = 8
+
+//Second way to declare variable
+//Is going to be used only in scope where you declared that
+let ourName = "UNIC"
+
+//Third way to declare variable
+//Cannot be changed (Regular const)
+const pi = 3.14
