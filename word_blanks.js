060b33a1a08a3992ce1fd3b25a51f04526a945ff
RAYNingTime <ivan0kosyakov@gmail.com>
2022-06-23 11:44:04 +0300

    Function

---
+++ word_blanks.js
@@ -0,0 +1,13 @@
+//Function
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+	// Your code below this line
+	var result = "";
+
+	result += "The " + myAdjective + " " + myNoun + " " + myVerb + " to the store " +  myAdverb;
+
+	// Your code above this line
+	return result;
+}
+
+console.log(wordBlanks("dog", "big", "ran", "quickly"));
+console.log(wordBlanks("bike", "slow", "flew", "slowly"));
