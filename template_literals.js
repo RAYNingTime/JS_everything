92a3dcc2d1145955230402706d2d31a45da0dd65
RAYNingTime <ivan0kosyakov@gmail.com>
2022-06-24 10:44:12 +0300

    Creating strings using Template Literals

---
+++ template_literals.js
@@ -0,0 +1,12 @@
+//Creating strings using Template Literals
+
+const person = {
+	name: "Zodiac Hasbro",
+	age: 56
+}
+
+//Template literal with multi-line and string interpolation
+const greeting = `Hello, my name is ${person.name}!
+I am ${person.age} years old.`;
+
+console.log(greeting);
