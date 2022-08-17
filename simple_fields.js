ed3587ec2932dedb8a85b134556815c5973800b5
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-08-17 13:16:45 +0300

    Update simple_fields.js (#11)
    Co-authored-by: Ivan(Janis) Ivanou <referdodanne@gmail.com>
    
    Co-authored-by: Ivan(Janis) Ivanou <referdodanne@gmail.com>

--- simple_fields.js
+++ simple_fields.js
@@ -9,6 +9,5 @@ const createPerson = (name,age,gender) => {
 //Same as 
 const createNewPerson = (name,age,gender) => ({ name, age, gender});
 
-
 console.log(createPerson("Zodiac Hasbro", 56, "male"));
 console.log(createNewPerson("Zodiac Hasbro", 56, "male"));
